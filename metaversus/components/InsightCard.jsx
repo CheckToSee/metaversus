ec61e879fbdfbc7a3c3b2321df9db7cc9ed8e722
CheckToSee <138322844+CheckToSee@users.noreply.github.com>
2023-08-29 22:59:49 -0400

    Initial commit

---
+++ metaversus/components/InsightCard.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../utils/motion";
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <div>
+    Insight Card
+  </div>
+);
+
+export default InsightCard;
